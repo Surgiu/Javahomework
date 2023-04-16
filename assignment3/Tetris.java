96d4c60453de320031be3d7947af54252c9138da
鼠鼠 <2824174663@qq.com>
2023-04-16 20:54:24 +0800

    增加了消去多行的功能

--- assignment3/Tetris.java
+++ assignment3/Tetris.java
@@ -12,7 +12,6 @@ public class Tetris {
     }
 
     public static int[][] generateMap(Scanner s) {
-        //生成最上方有两排缓冲区，最下方全为1的初始地图
         int row = s.nextInt() + 3;
         int col = s.nextInt();
         int[][] map = new int[row][col];
@@ -23,106 +22,105 @@ public class Tetris {
     }
 
     public static int[][] modify(Scanner s, int[][] m) {
-        int num = s.nextInt();//块的数量
+        int num = s.nextInt();
         for (int i = 0; i < num; i++) {
-            char target = s.next().charAt(0);//块的形状
-            int position = s.nextInt();//块的最左端的坐标
+            char target = s.next().charAt(0);
+            int position = s.nextInt();
             int rowIndex = 0;
-            {
-                if (target == 'I') {//目标块落入了地图
-                    label:
-                    for (int j = 2; j < m.length; j++) {
-                        for (int k = position; k < 4; k++) {
-                            if (m[j][k] == 1) {
-                                rowIndex = j - 1;
-                                break label;
-                            }
-                        }
-                    }
-                    for (int j = position; j < position + 4; j++) {
-                        m[rowIndex][j] = 1;
-                    }
-                } else if (target == 'O') {
-                    label:
-                    for (int j = 2; j < m.length; j++) {
-                        for (int k = position; k < position + 2; k++) {
-                            if (m[j][k] == 1) {
-                                rowIndex = j - 1;
-                                break label;
-                            }
-                        }
-                    }
-                    for (int j = rowIndex - 1; j <= rowIndex; j++) {
-                        for (int k = position; k < position + 2; k++) {
-                            m[j][k] = 1;
-                        }
-                    }
-                } else if (target == 'L') {
-                    label:
-                    for (int j = 2; j < m.length; j++) {
-                        for (int k = position; k < position + 2; k++) {
-                            if (m[j][k] == 1) {
-                                rowIndex = j - 1;
-                                break label;
-                            }
-                        }
-                    }
-                    m[rowIndex][position] = 1;
-                    m[rowIndex][position + 1] = 1;
-                    m[rowIndex - 1][position] = 1;
-                    m[rowIndex - 2][position] = 1;
-                } else if (target == 'J') {
-                    l:
-                    for (int j = 2; j < m.length; j++) {
-                        for (int k = position; k < position + 2; k++) {
-                            if (m[j][k] == 1) {
-                                rowIndex = j - 1;
-                                break l;
-                            }
-                        }
-                    }
-                    m[rowIndex][position] = 1;
-                    m[rowIndex][position + 1] = 1;
-                    m[rowIndex - 1][position + 1] = 1;
-                    m[rowIndex - 2][position + 1] = 1;
-                } else if (target == 'T') {
-                    for (int j = 2; j < m.length; j++) {
-                        if (m[j][position] == 1 || m[j + 1][position + 1] == 1 || m[j][position + 2] == 1) {
+            if (target == 'I') {
+                label:
+                for (int j = 2; j < m.length; j++) {
+                    for (int k = position; k < 4; k++) {
+                        if (m[j][k] == 1) {
                             rowIndex = j - 1;
-                            break;
+                            break label;
                         }
                     }
-                    for (int j = position; j < position + 3; j++) {
-                        m[rowIndex][j] = 1;
-                    }
-                    m[rowIndex + 1][position + 1] = 1;
-                } else if (target == 'S') {
-                    for (int j = 2; j < m.length; j++) {
-                        if (m[j][position] == 1 || m[j][position + 1] == 1 || m[j - 1][position + 2] == 1) {
-                            rowIndex = j - 1;
-                            break;
-                        }
-                    }
-                    m[rowIndex][position] = 1;
-                    m[rowIndex][position + 1] = 1;
-                    m[rowIndex - 1][position] = 1;
-                    m[rowIndex - 1][position + 1] = 1;
-                } else if (target == 'Z') {
-                    for (int j = 2; j < m.length; j++) {
-                        if (m[j][position] == 1 || m[j + 1][position + 1] == 1 || m[j + 1][position + 2] == 1) {
-                            rowIndex = j - 1;
-                            break;
-                        }
-                    }
-                    m[rowIndex][position] = 1;
-                    m[rowIndex][position + 1] = 1;
-                    m[rowIndex + 1][position + 1] = 1;
-                    m[rowIndex + 1][position + 2] = 1;
-
                 }
+                for (int j = position; j < position + 4; j++) {
+                    m[rowIndex][j] = 1;
+                }
+            } else if (target == 'O') {
+                label:
+                for (int j = 2; j < m.length; j++) {
+                    for (int k = position; k < position + 2; k++) {
+                        if (m[j][k] == 1) {
+                            rowIndex = j - 1;
+                            break label;
+                        }
+                    }
+                }
+                for (int j = rowIndex - 1; j <= rowIndex; j++) {
+                    for (int k = position; k < position + 2; k++) {
+                        m[j][k] = 1;
+                    }
+                }
+            } else if (target == 'L') {
+                label:
+                for (int j = 2; j < m.length; j++) {
+                    for (int k = position; k < position + 2; k++) {
+                        if (m[j][k] == 1) {
+                            rowIndex = j - 1;
+                            break label;
+                        }
+                    }
+                }
+                m[rowIndex][position] = 1;
+                m[rowIndex][position + 1] = 1;
+                m[rowIndex - 1][position] = 1;
+                m[rowIndex - 2][position] = 1;
+            } else if (target == 'J') {
+                l:
+                for (int j = 2; j < m.length; j++) {
+                    for (int k = position; k < position + 2; k++) {
+                        if (m[j][k] == 1) {
+                            rowIndex = j - 1;
+                            break l;
+                        }
+                    }
+                }
+                m[rowIndex][position] = 1;
+                m[rowIndex][position + 1] = 1;
+                m[rowIndex - 1][position + 1] = 1;
+                m[rowIndex - 2][position + 1] = 1;
+            } else if (target == 'T') {
+                for (int j = 2; j < m.length; j++) {
+                    if (m[j][position] == 1 || m[j + 1][position + 1] == 1 || m[j][position + 2] == 1) {
+                        rowIndex = j - 1;
+                        break;
+                    }
+                }
+                for (int j = position; j < position + 3; j++) {
+                    m[rowIndex][j] = 1;
+                }
+                m[rowIndex + 1][position + 1] = 1;
+            } else if (target == 'S') {
+                for (int j = 2; j < m.length; j++) {
+                    if (m[j][position] == 1 || m[j][position + 1] == 1 || m[j - 1][position + 2] == 1) {
+                        rowIndex = j - 1;
+                        break;
+                    }
+                }
+                m[rowIndex][position] = 1;
+                m[rowIndex][position + 1] = 1;
+                m[rowIndex - 1][position] = 1;
+                m[rowIndex - 1][position + 1] = 1;
+            } else if (target == 'Z') {
+                for (int j = 2; j < m.length; j++) {
+                    if (m[j][position] == 1 || m[j + 1][position + 1] == 1 || m[j + 1][position + 2] == 1) {
+                        rowIndex = j - 1;
+                        break;
+                    }
+                }
+                m[rowIndex][position] = 1;
+                m[rowIndex][position + 1] = 1;
+                m[rowIndex + 1][position + 1] = 1;
+                m[rowIndex + 1][position + 2] = 1;
+
             }
+
             m = eliminate(m);
-            if (checkEnd(m)) {//判断是否结束游戏
+            if (checkEnd(m)) {
                 return m;
             }
         }
@@ -130,21 +128,37 @@ public class Tetris {
     }
 
     public static int[][] eliminate(int[][] a) {
-        for (int i = 2; i < a.length - 1; i++) {//检测
-            int count = 0;
-            for (int j = 0; j < a[i].length; j++) {
-                if (a[i][j] == 1) {
-                    count++;
+        int w = -1;
+        do {
+            for (int i = 2; i < a.length - 1; i++) {
+                int count = 0;
+                for (int j = 0; j < a[i].length; j++) {
+                    if (a[i][j] == 1) {
+                        count++;
+                    }
                 }
-            }
-            if (count == a[i].length) {
-                for (int k = i; k > 0; k--) {//拷贝待删行上方的地图
-                    for (int l = 0; l < a[k].length; l++) {
-                        a[k][l] = a[k - 1][l];
+                if (count == a[i].length) {
+                    for (int k = i; k > 0; k--) {
+                        for (int l = 0; l < a[k].length; l++) {
+                            a[k][l] = a[k - 1][l];
+                        }
                     }
                 }
             }
-        }
+            int n = 0;
+            l:
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
+                    if (a[i][j] == 1) {
+                        n++;
+                    }
+                    if (n == a[i].length) {
+                        w++;
+                        break l;
+                    }
+                }
+            }
+        } while (w == -1);
         return a;
     }
 
